7509bac9133826d0d8cc1d72ae8fa8623698d93c
sjpark-dev <sjpark.dev@gmail.com>
2021-07-28 16:39:31 +0900

    chore: 쉘 스크립트 테스트

--- auto-script-gm.sh
+++ auto-script-gm.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "> now ing app pid find!"
-CURRENT_PIDS=$(pgrep -f start-gpu-manager.py)
+CURRENT_PIDS=$(ps -ef | grep start-gpu-manager.py)
 if [ -z "$CURRENT_PIDS" ]; then
-        echo "> no ing app."
+  echo "> no ing app."
 else
-        echo "$CURRENT_PIDS"
-        for PID in $CURRENT_PIDS; do
-                echo "> kill -9 $PID"
-                kill -9 $PID
-                sleep 3
-        done
+  echo "$CURRENT_PIDS"
+  for PID in $CURREND_PIDS; do
+    echo "> kill -9 $PID"
+    kill -9 "$PID"
+    sleep 3
+  done
 fi
 echo "> new app deploy"
-nohup python3 start-gpu-manager.py &
+nohup python3 start-gpu-mangaer.py &
 sleep 3
