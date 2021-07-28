565d908b4b1263a8e2eb2c07c6e288538781882e
sjpark-dev <sjpark.dev@gmail.com>
2021-07-28 15:41:09 +0900

    쉘 스크립트 수정

--- auto-script-gm.sh
+++ auto-script-gm.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 echo "> now ing app pid find!"
-CURRENT_PID=$(pgrep -f start-gpu-manager.py)
-echo "$CURRENT_PID"
-if [ -z $CURRENT_PID ]; then
+CURRENT_PIDS=$(pgrep -f start-gpu-manager.py)
+if [ -z "$CURRENT_PIDS" ]; then
         echo "> no ing app."
 else
-        echo "> kill -9 $CURRENT_PID"
-        kill -9 $CURRENT_PID
-        sleep 3
+        echo "$CURRENT_PIDS"
+        for PID in $CURRENT_PIDS; do
+                echo "> kill -9 $PID"
+                kill -9 $PID
+                sleep 3
+        done
 fi
 echo "> new app deploy"
-PY_NAME=$(ls |grep 'gpu-manager' | tail -n 1)
-echo "> PY Name: $PY_NAME"
-
-nohup python3 $PY_NAME &
+nohup python3 start-gpu-manager.py &
 sleep 3
