41d887c97df90f2ecd251c146cf6110cfe3f5168
sjpark-dev <sjpark.dev@gmail.com>
2021-07-28 16:53:57 +0900

    chore: 쉘 스크립트 수정

--- auto-script-gm.sh
+++ auto-script-gm.sh
@@ -5,7 +5,7 @@ if [ -z "$CURRENT_PIDS" ]; then
   echo "> no ing app."
 else
   echo "$CURRENT_PIDS"
-  for PID in $CURREND_PIDS; do
+  for PID in $CURRENT_PIDS; do
     echo "> kill -9 $PID"
     kill -9 "$PID"
     sleep 3
